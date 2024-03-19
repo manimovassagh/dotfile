b54c36e7735effc691328601c4ee1a37068edc55
manimovassagh <mani.mgh@gmail.com>
2024-03-19 12:01:11 +0100

    remove auto import

--- lua/plugins/auto-import.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "stevanmilic/nvim-lspimport",
-    config = function()
-        vim.keymap.set("n", "<leader>a", require("lspimport").import, {
-            noremap = true
-        })
-    end
-}
--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -9,13 +9,15 @@ config = function() -- This is the function that runs, AFTER loading
     ['<leader>ff'] = { name = 'format code', _ = 'which_key_ignore' },
     ['<leader>f'] = { name = 'Format and grep f,g', _ = 'which_key_ignore' },
     ['<leader>fg'] = { name = 'grep', _ = 'which_key_ignore' },
-    ['<leader>g'] = { name = 'Go to definition with gd and gr', _ = 'which_key_ignore' },
+    ['<leader>g'] = { name = 'Go to definition with gd and gr and comment', _ = 'which_key_ignore' },
     ['<leader>gd'] = { name = 'Go to definition', _ = 'which_key_ignore' },
     ['<leader>gr'] = { name = 'Go to refrence', _ = 'which_key_ignore' },
     ['<leader>c'] = { name = 'Code action ca', _ = 'which_key_ignore' },
     ['<leader>ca'] = { name = 'Code action with ca', _ = 'which_key_ignore' },
     ['<leader>b'] = { name = 'Go to buffers on neotree', _ = 'which_key_ignore' },
     ['<leader>t'] = { name = 'Terminal toggle', _ = 'which_key_ignore' },
+    ['<leader>gcc'] = { name = 'Comment Toggle', _ = 'which_key_ignore' },
+    ['<leader>gc'] = { name = 'Comment Toggle', _ = 'which_key_ignore' },
     
 
   }
