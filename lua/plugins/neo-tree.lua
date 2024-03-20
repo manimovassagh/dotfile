17f22a78d9e0c2d64b00ce5bdddd412f64467490
Mani Movassagh <74688476+manimovassagh@users.noreply.github.com>
2024-03-20 01:32:23 +0100

    Update neo-tree.lua

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -5,6 +5,14 @@ return {
     "MunifTanjim/nui.nvim" -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     },
     config = function()
+            require("neo-tree").setup({
+            filesystem = {
+                follow_current_file = {
+                    enabled = true, -- This will find and focus the file in the active buffer every time
+                    leave_dirs_open = true -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+                }
+            }
+        })
         vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>', {
             silent = true
         })
